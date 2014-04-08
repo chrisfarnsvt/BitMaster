5a94e782d4b9dfb4e67d99f19e327ca5e6a65ea8
Colin Bates <colinbatesvt@gmail.com>
2014-04-08 09:38:19 -0400

    start screen functioning

--- src/com/example/bitmaster/MainActivity.java
+++ src/com/example/bitmaster/MainActivity.java
@@ -2,14 +2,16 @@ package com.example.bitmaster;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.game_layout);
+		setContentView(R.layout.start_layout);
 	}
 
 	@Override
@@ -17,6 +19,17 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+	
+	public void startGame(View Button) {
+		Intent gameActivity = new Intent(getBaseContext(), GameActivity.class);
+		startActivity(gameActivity);
+		
+	}
+	
+	public void startOptions(View Button) {
+	//	Intent settingsActivity = new Intent(getBaseContext(), PrefActivity.class);
+		//startActivity(settingsActivity);
 	}
 
 }
