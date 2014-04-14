cd93f36901795f0a0230057f393d7a406ce55e6d
Colin Bates <colinbatesvt@gmail.com>
2014-04-14 10:49:24 -0400

    stuff in gameActivity

--- src/com/example/bitmaster/GameActivity.java
+++ src/com/example/bitmaster/GameActivity.java
@@ -1,9 +1,12 @@
 package com.example.bitmaster;
 
+import java.util.Random;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 public class GameActivity extends Activity{
 	@Override
@@ -11,12 +14,47 @@ public class GameActivity extends Activity{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.game_layout);
 		FrameLayout fl = (FrameLayout) findViewById(R.id.frameLayout);
+		
 		_size = 8;
-		Grid g = new Grid(this, _size);
+		grid = new Grid(this, _size);
+		
 		FrameLayout.LayoutParams p = new FrameLayout.LayoutParams(50*_size,50*_size, Gravity.CENTER_HORIZONTAL);
-		fl.addView(g,p);
-		//work please
+		fl.addView(grid,p);
+		
+		goal = (TextView) findViewById(R.id.goal);
+		goal.setText(String.valueOf(0));
+		
+		current = (TextView)findViewById(R.id.current);
+		String bin = grid.getBin(); 
+		int dec = 0;
+		for(int i = 0; i<bin.length(); i++) {
+			if(Integer.valueOf(bin.substring(i, i+1)) == 0)
+				dec = dec * 2;
+			else
+				dec = (dec * 2) +1;
+		}
+		
+		current.setText(String.valueOf(dec));
 	}
 	
+	public void setAnswers(int[] a) {
+		_answers = a;
+	}
+	
+	public void update(Boolean result) {
+		current.setText(String.valueOf(grid.getBin()));
+		if (result){
+			_count ++;
+			goal.setText(String.valueOf(_answers[_count]));
+		}
+			
+	}
+	
+	TextView goal;
+	TextView current;
+	Grid grid;
+	private int[] _answers;
 	private int _size;
+	private Random _random;
+	private int _count;
 }
