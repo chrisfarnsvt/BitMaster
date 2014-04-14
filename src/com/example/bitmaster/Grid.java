60c8d04db25913a646a2f007739e1257079b4b7e
Chris Farnsworth <chrisfarnsvt@gmail.com>
2014-04-14 09:48:55 -0400

    Knock-down, DRAG-out dragging

--- src/com/example/bitmaster/Grid.java
+++ src/com/example/bitmaster/Grid.java
@@ -2,6 +2,7 @@ package com.example.bitmaster;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
@@ -15,7 +16,11 @@ public class Grid extends TableLayout implements View.OnTouchListener {
 		super(context);
 		_context = context;
 		_side = side;
+		_bin = "";
 		_bins = new int[side][side];
+		_curCell = new int[2];
+		_prevCell = new int[2];
+		_selCoords = new int[side][side];
 		_views = new TextView[side][side];
 		fillTable(_context, _side);
 		setOnTouchListener(this);
@@ -45,24 +50,82 @@ public class Grid extends TableLayout implements View.OnTouchListener {
 		switch(event.getAction()){		
 			case MotionEvent.ACTION_MOVE:
 				{
-		
+					int x = (int) event.getX() / 50;
+					int y = (int) event.getY() / 50;
+					if (x > (_side - 1) || y > (_side - 1) || x < 0 || y < 0)
+						return true;
+					if (x != _curCell[0] || y != _curCell[1]) {
+						_prevCell[0] = _curCell[0];
+						_prevCell[1] = _curCell[1];
+						_curCell[0] = x;
+						_curCell[1] = y;
+						if (_selCoords[x][y] == 1) {
+							_views[_prevCell[1]][_prevCell[0]].setBackgroundColor(Color.YELLOW);
+							_views[y][x].setBackgroundColor(Color.YELLOW);
+							_selCoords[x][y] = 0;
+							_selCoords[_prevCell[1]][_prevCell[0]] = 0;
+							return true;
+						}
+						Log.i("information", "move called");
+						_selCoords[x][y] = 1;
+						_views[y][x].setBackgroundColor(Color.RED);
+						return true;
+					}
+					return false;
 				}
 	
 			case MotionEvent.ACTION_DOWN:
-				{
-					_views[(int) (event.getY()/50)][(int) (event.getX()/50)].setBackgroundColor(Color.RED);
+			{
+				int x = (int) event.getX() / 50;
+				int y = (int) event.getY() / 50;
+				if (x > (_side - 1) || y > (_side - 1) || x < 0 || y < 0)
+					return true;
+				if (x != _curCell[0] || y != _curCell[1]) {
+						_prevCell[0] = _curCell[0];
+						_prevCell[1] = _curCell[1];
+						_curCell[0] = x;
+						_curCell[1] = y;
+						if (_selCoords[x][y] == 1) {
+							_views[_prevCell[1]][_prevCell[0]].setBackgroundColor(Color.YELLOW);
+							_views[y][x].setBackgroundColor(Color.YELLOW);
+							_selCoords[x][y] = 0;
+							_selCoords[_prevCell[1]][_prevCell[0]] = 0;
+							return true;
+						}
+						Log.i("information", "move called");
+						_selCoords[x][y] = 1;
+						_views[y][x].setBackgroundColor(Color.RED);
+						return true;
+					}
+					return false;
 				}	
 	
 			case MotionEvent.ACTION_UP:
 				{
-		
+					Log.i("info", "up called");
+					for (int i = 0; i < _side; i++) {
+						for (int n = 0; n < _side; n++) {
+							if (_selCoords[n][i] == 1) {
+									_views[i][n].setBackgroundColor(Color.YELLOW);
+									_bin += _views[i][n].getText();
+									_selCoords[n][i] = 0;
+							}
+						}
+					}
+					return true;
 				}
-		}
-		return true;
+			}
+				return false;
 	}
+	
+	
 
+	private int[] _curCell;
+	private int[] _prevCell;
 	private int[][] _bins;
+	private String _bin;
 	private TextView[][] _views;
+	private int[][] _selCoords;
 	private Context _context;
 	private int _side;
 	//private TableLayout _table;
